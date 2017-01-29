14b6f70581f1642bf34d5a78d20e37f6d3b920f7
Franck Brazier <franck.brazier@telecom-paristech.fr>
2017-01-29 02:22:27 -0500

    Erreur d'Apa corrigee

--- Python/main.py
+++ Python/main.py
@@ -10,13 +10,14 @@ import sql,json,sys
 
 def main(text,tMax,nBest):
     activity_set = recuperer()
-    resultat = journey_optimizer_master(activity_set, tMax, nBest) 
+    resultat = journey_optimizer_stochastic(activity_set, tMax, nBest) 
     printable  = '['
     #print resultat
     if len(resultat)>0:
-        for x in resultat:
+        x = resultat[0]
             #print x.toJSON()
-            printable += x.toJSON()+','
+        for y in x:
+            printable += y.toJSON()+','
         printable = printable[:-1]
         print printable+']'
 
@@ -25,7 +26,7 @@ if __name__ == "__main__":
     from travel_generator import *
     import json,sys
     if len(sys.argv) > 1 : 
-        print sys.argv
+        #print sys.argv
         main(sys.argv[1],60*int(sys.argv[2]),int(sys.argv[3]))
     else:
         main('',6*60,1)
